27b0925c531ae197e9163ff4a4837fa61f430e24
Justwouter <wouter2312@gmail.com>
2022-04-26 11:53:21 +0200

    Test add

--- src/test/java/JavaFXAppTest.java
+++ src/test/java/JavaFXAppTest.java
@@ -1,9 +1,15 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions;
 
 class JavaFXAppTest {
     @Test
     void aDummyTest() {
         assertEquals(0,0);
+    }
+    
+    @Test
+    void addTest(){
+        Assertions.assertEquals(3, new JavaFXApp().computeAdd(1, 2));
     }
 }
