1284c83594a70d4770e82e4d92d7f6ca1b89ed2c
Lucas <LucasClavel356@gmail.com>
2022-04-26 11:50:41 +0200

    testComputeDivide toegevoegd

--- src/test/java/JavaFXAppTest.java
+++ src/test/java/JavaFXAppTest.java
@@ -6,4 +6,12 @@ class JavaFXAppTest {
     void aDummyTest() {
         assertEquals(0,0);
     }
+    
+    public void testComputeDivide(){
+        int nummer1 = 10;
+        int nummer2 = 10;
+        int nummerTest = 1;
+        
+        assertEquals(nummerTest, new JavaFXApp().computeDivide(nummer1, nummer2));
+    }
 }
