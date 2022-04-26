85de19a4812f12c0f9cd42093c5580f16477157c
WesselMH <w.m.horsthuis@live.nl>
2022-04-26 11:48:30 +0200

    Testmult

--- src/test/java/JavaFXAppTest.java
+++ src/test/java/JavaFXAppTest.java
@@ -1,9 +1,18 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.Assertions;
+
 class JavaFXAppTest {
     @Test
     void aDummyTest() {
         assertEquals(0,0);
+    }
+
+    @Test
+    void test(){
+        JavaFXApp Multiply = new JavaFXApp();
+        Assertions.assertEquals(6, Multiply.computeMultiply(2, 3));        
+
     }
 }
